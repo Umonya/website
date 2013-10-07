ce685e3abc908be10fe4878e27c6b7beb5b9840b
Rizmari Versfeld <rizmari@praekeltconsulting.com>
2013-10-07 14:07:11 +0200

    manage form via registration prefs, display form link in nav bar

--- website/apps/core/models.py
+++ website/apps/core/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
 
+from preferences import preferences
 from preferences.models import Preferences
+from forms_builder.forms.models import Form, STATUS_PUBLISHED, STATUS_DRAFT
 
 
 class ContactPreferences(Preferences):
@@ -32,3 +35,57 @@ class ContactPreferences(Preferences):
     def emails(self):
         user_emails = list(self.email_recipients.values_list('email', flat=True))
         return user_emails + self.emails_from_raw
+
+
+class StudentRegistrationPreferences(Preferences):
+    __module__ = 'preferences.models'
+    registration_form = models.ForeignKey(
+        Form,
+        null=True,
+        blank=True,
+        help_text="Students will use this form to register for a course."
+    )
+    registration_status = models.CharField(
+        max_length=8,
+        default='closed',
+        choices=(
+            ('open', 'Open'),
+            ('closed', 'Closed')
+        )
+    )
+
+    class Meta:
+        verbose_name_plural = 'student registration preferences'
+
+
+def set_form_status(sender, **kwargs):
+    '''
+    Publish/retract registration form based on the
+    current registration status. The form url should
+    return 404 if registrations are closed.
+    '''
+    try:
+        if sender is StudentRegistrationPreferences:
+            reg_prefs = kwargs['instance']
+            form = reg_prefs.registration_form
+        else:
+            reg_prefs = preferences.StudentRegistrationPreferences
+            form = kwargs['instance']
+            if reg_prefs.registration_form_id != form.id:
+                return
+
+        if form.status == STATUS_DRAFT and \
+                reg_prefs.registration_status == 'open':
+            form.status = STATUS_PUBLISHED
+            form.save(update_fields=['status'])
+        elif form.status == STATUS_PUBLISHED and \
+                reg_prefs.registration_status == 'closed':
+            form.status = STATUS_DRAFT
+            form.save(update_fields=['status'])
+        
+    except StudentRegistrationPreferences.DoesNotExist:
+        pass
+
+
+post_save.connect(set_form_status, sender=StudentRegistrationPreferences)
+post_save.connect(set_form_status, sender=Form)
--- website/settings/common.py
+++ website/settings/common.py
@@ -122,6 +122,7 @@ TEMPLATE_DIRS = (
 from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS as TCP
 TEMPLATE_CONTEXT_PROCESSORS = TCP + (
     'django.core.context_processors.request',
+    'website.apps.core.context_processors.registration'
 )
 
 INSTALLED_APPS = (
